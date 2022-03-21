3ca1f7f52cf4e9e9b1261dbc58c5db1ab8edec6e
Tsepo-Matheleli <tsepo.matheleli@umuzi.org>
2022-03-21 15:56:36 +0200

    Updated naming convention for functions.

--- insta_unfollower.py
+++ insta_unfollower.py
@@ -12,31 +12,31 @@ class instagram_unfollow:
         self.driver = webdriver.Firefox(executable_path=r"drivers/geckodriver")
 
     def login(self):
-        field_username = self.find_element(by.CSS_SELECTOR, "input[name='username']")
-        field_password = self.find_element(by.CSS_SELECTOR, "input[name='password']")
+        field_username = self.execute_element(by.CSS_SELECTOR, "input[name='username']")
+        field_password = self.execute_element(by.CSS_SELECTOR, "input[name='password']")
         field_username.clear()
         field_username.send_keys(self.username)
         field_password.clear()
         field_password.send_keys(self.password)
-        self.find_element(by.CSS_SELECTOR, "button[type = 'submit']", True)
+        self.execute_element(by.CSS_SELECTOR, "button[type = 'submit']", True)
 
-    def get_my_user_account(self):
+    def get_main_account(self):
         # Removes the not now pop ups that show when login into instagram
         # self.find_element(by.XPATH, "//button[contains(text(), 'Not Now')]", True)
         # self.find_element(by.XPATH, "//button[contains(text(), 'Not Now')]", True)
-        self.find_element(by.XPATH, f"//a[@href = '/{self.username}/']", True)
+        self.execute_element(by.XPATH, f"//a[@href = '/{self.username}/']", True)
 
-    def get_people_who_follow_me(self):
+    def get_main_account_followers(self):
         followers_link_x_path = (
             "/html/body/div[1]/section/main/div/header/section/ul/li[2]"
         )
-        self.find_element(by.XPATH, followers_link_x_path, True)
+        self.execute_element(by.XPATH, followers_link_x_path, True)
         # Get list of all the people who are followers
         self.driver.execute_script("window.scrollTo(0,5000);")
-        followers_list = self.find_element(by.CLASS_NAME, "PZuss", False, 60)
+        followers_list = self.execute_element(by.CLASS_NAME, "PZuss", False, 60)
         print(followers_list)
 
-    def find_element(self, method, path, button=False, wait_time=15):
+    def execute_element(self, method, path, button=False, wait_time=15):
         if button == False:
             return wait(self.driver, wait_time).until(
                 condition.element_to_be_clickable((method, path))
